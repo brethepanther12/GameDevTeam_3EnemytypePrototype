30b11dfe5c8c1a9521415f9bdf44ead770fe4f74
brethepanther12 <155138278+brethepanther12@users.noreply.github.com>
2025-07-01 18:56:38 -0400

    WorkHook test

---
+++ Assets/Scripts/Temp(Test WorkHooks).cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Temp(TestWorkHooks) : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //This is just a temporary push to test WorkHooks
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
