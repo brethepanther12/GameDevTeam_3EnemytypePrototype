7028b5de6f7d2dbbdb3047c4db6d0f3e125c6473
brethepanther12 <155138278+brethepanther12@users.noreply.github.com>
2025-07-01 20:11:54 -0400

    Update Temp(Test WorkHooks).cs

--- Assets/Scripts/Temp(Test WorkHooks).cs
+++ Assets/Scripts/Temp(Test WorkHooks).cs
@@ -1,11 +1,15 @@
 using UnityEngine;
+using System.Collections;
 
 public class Temp : MonoBehaviour
 {
+    public [SerializeField] int tempvar;
+    public int five = 5;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         //This is just a temporary push to test WorkHooks
+        tempvar = five;
     }
 
     // Update is called once per frame
@@ -13,4 +17,6 @@ public class Temp : MonoBehaviour
     {
         
     }
+    
+    
 }
