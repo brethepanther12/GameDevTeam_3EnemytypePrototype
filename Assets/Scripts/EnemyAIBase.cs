bd74d4cca1d31d01048b7d7d7564a86fa28cf65d
brethepanther12 <155138278+brethepanther12@users.noreply.github.com>
2025-07-02 23:54:40 -0400

    Update EnemyAIBase.cs
    Created methods enemyFlashRead(), takeDamage(int amount), enemyFacePlayer(), OnTriggerEnter and Exit, enemyMoveToPlayer(), and enemyDeath().

--- Assets/Scripts/EnemyAIBase.cs
+++ Assets/Scripts/EnemyAIBase.cs
@@ -1,38 +1,125 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyAIBase : MonoBehaviour, IDamage
 {
     //Enemy health
-    protected [SerializeField] int enemyCurrentHealthPoints;
-    public [SerializeField] int enemyHealthPointsMax;
+    [SerializeField] protected int enemyCurrentHealthPoints;
+    [SerializeField] public int enemyHealthPointsMax;
+    //Enemy model
+    [SerializeField] public Renderer enemyModel;
+    protected Color enemyColorOrigin;
 
-    //Enemy movment
-    public [SerializeField] float enemySpeed;
+    //Enemy movement
+    [SerializeField] public float enemySpeed;
 
     //player detection
-    public [SerializeField] float enemyDetectionMeshRange;
-    protected Transform enemyPlayerObject;
+    //Navigation Mesh is used instead -v-
+    //public [SerializeField] float enemyDetectionMeshRange;
+     [SerializeField] public NavMeshAgent enemyNavAgent;
+    [SerializeField] public Transform enemyPlayerObject;
     protected bool enemyPlayerInSight;
-    
 
+
+    protected Vector3 enemyPlayerDirection;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     protected virtual void Start()
     {   //To save the enemy's max health to currently.
         enemyCurrentHealthPoints = enemyHealthPointsMax;
+
         //Assigning the object with "Player" string tag to the Transform var
         enemyPlayerObject = GameObject.FindGameObjectWithTag("Player").transform;
 
+        //Assigning the 3d vector of the player's position
+        enemyPlayerDirection = enemyPlayerObject.transform.position - transform.position;
+
+        //Fetching navigation mesh attached to 'this' game object
+        enemyNavAgent = GetComponent<NavMeshAgent>();
+
+        //This assigns the original color of the placed model in the Unity Inspector
+        enemyColorOrigin = enemyModel.material.color;
     }
 
     // Update is called once per frame
     protected virtual void Update()
     {
+        //Since the enemy is moving, it will constantly update
+        enemyMoveToPlayer();
+    }
+    protected virtual void enemyDeath()
+    {
+        //Debug will message the debugger that an enemy dies
+        //by getting the string name of the gameObject
+        Debug.Log($"{gameObject.name} has died");
+
+        //Then it destroy 'this' object after
+        Destroy(gameObject);
         
     }
-
-    public void takeDamage(int amount)
+    protected virtual void enemyMoveToPlayer()
     {
-       
+        //If check checks the toggled bool enemyPlayerInSight
+        if (enemyPlayerInSight)
+        {
+            //The navigation mesh calls SetDestination, which will be the Player's position
+            enemyNavAgent.SetDestination(enemyPlayerObject.transform.position);
+
+            //This check is for the enemy's position to the target is
+            //less than the allowed distance before stopping
+            if(enemyNavAgent.remainingDistance <= enemyNavAgent.stoppingDistance)
+            {
+                //This calls for the enemy to face forward to the player
+                enemyFacePlayer();
+            }
+        }
+
     }
 
+    //The OnTriggers toggle the player going in and out of the NavMesh
+    protected virtual void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            enemyPlayerInSight = true;
+        }
+    }
+    protected virtual void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            enemyPlayerInSight = false;
+        }
+    }
+
+    protected virtual void enemyFacePlayer()
+    {
+        //rotate holds the LookRotation() that creates
+        //a rotation based on a specific forward(z-axis) and upwards(y-axis)
+        // A Vector# can be placed with the x an y axis of the player's direction
+        // -For 'flying' enemy's the z can be placed in as well.
+        Quaternion rotate = Quaternion.LookRotation(new Vector3(enemyPlayerDirection.x, 0, enemyPlayerDirection.z));
+
+        //Lerp takes the estimated value of the rotation of the enemy and the player
+        transform.rotation = Quaternion.Lerp(transform.rotation, rotate, Time.deltaTime * enemySpeed);
+    }
+    public virtual void takeDamage(int amount)
+    {
+        enemyCurrentHealthPoints -= amount;
+        if (enemyCurrentHealthPoints <= 0)
+        {
+            enemyDeath();
+        }
+        else
+        {
+           StartCoroutine(enemyFlashRead());
+        }
+    }
+
+    protected virtual IEnumerator enemyFlashRead()
+    {
+        enemyModel.material.color = Color.red;
+        yield return new WaitForSeconds(0.2f);
+        enemyModel.material.color = enemyColorOrigin;
+    }
 }
