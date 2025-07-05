aba8d5daefbba2117e114551550529553d8c2817
FinsofFury <jowersjack@gmail.com>
2025-07-04 20:37:44 -0400

    Fully functioning main and options menu
    options menu can change fullscreen, has a master volume slider, music volume slider, and functioning back button. Both menus have placeholder music from the class audio file that I was using to test the sliders. I added code for both menus as well.

--- Assets/Scripts/MainMenuUI.cs
+++ Assets/Scripts/MainMenuUI.cs
@@ -1,8 +1,14 @@
+using JetBrains.Annotations;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuUI : MonoBehaviour
 {
+    [SerializeField] OptionsMenuUI optionMenuUI;
+    [SerializeField] GameObject optionsPanel;
+    [SerializeField] GameObject mainMenuPanel;
+
     public void startGame()
     {
         // I have this going to my scene for now for testing purposes.
@@ -20,7 +26,14 @@ public class MainMenuUI : MonoBehaviour
 
     public void openOptions()
     {
-        // this is a place holder
-        Debug.Log("Options opened");
+        optionMenuUI.InitializeOptions();
+        optionsPanel.SetActive(true);
+        mainMenuPanel.SetActive(false);
+    }
+    
+    public void closeOptions()
+    {
+        optionsPanel.SetActive(false);
+        mainMenuPanel.SetActive(true);
     }
  }
