e81b83da41103475968011e97dc7383d45771a8d
Christian Lykins <CALykins@student.fullsail.edu>
2025-07-05 17:24:05 -0400

    Project fixed

--- Assets/Scripts/IDamage.cs
+++ Assets/Scripts/IDamage.cs
@@ -2,10 +2,5 @@ using UnityEngine;
 
 public interface IDamage
 {
-<<<<<<< Updated upstream
     void takeDamage(int amount);
-
-=======
-    void TakeDamage(int amount);
->>>>>>> Stashed changes
 }
--- Assets/Scripts/cameraController.cs
+++ Assets/Scripts/cameraController.cs
@@ -2,23 +2,18 @@ using UnityEngine;
 
 public class cameraController : MonoBehaviour
 {
-<<<<<<< Updated upstream
-=======
 
->>>>>>> Stashed changes
     [SerializeField] int sens;
     [SerializeField] int lockVertMin, lockVertMax;
     [SerializeField] bool invertY;
 
     float rotX;
+   
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-<<<<<<< Updated upstream
-=======
-        
->>>>>>> Stashed changes
+
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
     }
@@ -26,28 +21,15 @@ public class cameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-<<<<<<< Updated upstream
+
         float mouseY = Input.GetAxis("Mouse Y") * sens * Time.deltaTime;
         float mouseX = Input.GetAxis("Mouse X") * sens * Time.deltaTime;
         if (invertY)
             rotX += mouseY;
         else
             rotX -= mouseY;
-=======
-        
-
-        float mouseY = Input.GetAxis("Mouse Y") * sens * Time.deltaTime;
-        float mouseX = Input.GetAxis("Mouse X") * sens * Time.deltaTime;
-
-        if (invertY)
-        {
-            rotX += mouseY;
-        } else
-        {
-            rotX -= mouseY;
-        }
             
->>>>>>> Stashed changes
+
 
         rotX = Mathf.Clamp(rotX, lockVertMin, lockVertMax);
 
@@ -55,9 +37,5 @@ public class cameraController : MonoBehaviour
 
         transform.parent.Rotate(Vector3.up * mouseX);
     }
-<<<<<<< Updated upstream
-=======
 
-    
->>>>>>> Stashed changes
 }
