200ea75e9fc3cb94f660a79f29a8d0dd07bb7172
Travis-Huth94 <travishuth84@gmail.com>
2025-07-10 01:12:13 -0500

    Boss Mechanics Completed

--- Assets/Scripts/BossAI.cs
+++ Assets/Scripts/BossAI.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using UnityEngine;
 
-public class BossAI : EnemyAIBase, IDamage
+public class BossAI : EnemyAIBase
 {
     [Header("Boss Settings")]
     [SerializeField] GameObject projectilePrefab;
