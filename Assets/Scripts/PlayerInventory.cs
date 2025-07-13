684403e7243f9c4c8823749a4e9a2005ff898162
FinsofFury <jowersjack@gmail.com>
2025-07-13 12:19:01 -0400

    made Inventory list for player so I can start work on the new unlock system

---
+++ Assets/Scripts/PlayerInventory.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class PlayerInventory : MonoBehaviour
+{
+    public List<ItemSO> collectedItems = new List<ItemSO>();
+
+    public void AddItem(ItemSO item)
+    {
+        if (!collectedItems.Contains(item))
+        {
+            collectedItems.Add(item);
+            Debug.Log("Picked up: " + item.itemName);
+        }
+    }
+
+    public bool HasAllItems(List<ItemSO> requiredItems)
+    {
+        foreach (ItemSO item in requiredItems)
+        {
+            if (!collectedItems.Contains(item))
+             return false;
+        }
+        return true;
+    }
+    
+    public bool HasItem(ItemSO item)
+    {
+        return collectedItems.Contains(item);
+    }
+}
