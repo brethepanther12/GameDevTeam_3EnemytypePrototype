8c051997c0dd0321e13a27f894c9b280c6d6b338
brethepanther12 <155138278+brethepanther12@users.noreply.github.com>
2025-07-15 20:53:54 -0400

    Added movement and implemented FixedUpdate()

--- Assets/Scripts/FlyingAI.cs
+++ Assets/Scripts/FlyingAI.cs
@@ -2,15 +2,37 @@ using UnityEngine;
 
 public class FlyingAI : MonoBehaviour
 {
+    [SerializeField] private Transform target;
+    private GameObject playerTarget;
+    [SerializeField] private float flyingSpeed;
+    [SerializeField] private float rotationSpeed;
+
+    [SerializeField] private Rigidbody rigidBody;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        if (rigidBody == null) rigidBody = GetComponent<Rigidbody>();
+
+        playerTarget = gamemanager.instance.player;
+
+        if (playerTarget != null) target = playerTarget.transform;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (target == null) return;
+
+        // Direction toward the target
+        Vector3 direction = (target.position - transform.position).normalized;
+
+        // Smooth rotation
+        Quaternion targetRot = Quaternion.LookRotation(direction);
+        rigidBody.MoveRotation(Quaternion.Slerp(rigidBody.rotation, targetRot, rotationSpeed * Time.fixedDeltaTime));
+
+        // Move forward
+        rigidBody.linearVelocity = transform.forward * flyingSpeed;
     }
 }
