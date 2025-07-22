6b73c038a174d6f9f1823ab3076a7275565e5a2e
brethepanther12 <155138278+brethepanther12@users.noreply.github.com>
2025-07-22 18:21:16 -0400

    Update Grenade.cs
    added explosion method, creating and instating a prefab within the specific inputted time frame.

--- Assets/Scripts/Grenade.cs
+++ Assets/Scripts/Grenade.cs
@@ -1,11 +1,21 @@
 using UnityEngine;
+using System.Collections;
 
 public class Grenade : MonoBehaviour
 {
+    [SerializeField] private Rigidbody grenadeRigidB;
+
+    [SerializeField] private int grenadeSpeed;
+    [SerializeField] private int grenadeSpeedY;
+    [SerializeField] private float destroyTimer;
+
+    [SerializeField] private GameObject explosionPrefab;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        grenadeRigidB.linearVelocity = (transform.forward * grenadeSpeed) + (transform.up * grenadeSpeedY);
     }
 
     // Update is called once per frame
@@ -13,4 +23,12 @@ public class Grenade : MonoBehaviour
     {
         
     }
+
+    IEnumerable explode()
+    {
+        yield return new WaitForSeconds(destroyTimer);
+        Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
 }
+
