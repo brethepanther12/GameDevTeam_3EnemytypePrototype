56c0456b0fc4b7b264cdcb60055e5457d847c6e6
brethepanther12 <155138278+brethepanther12@users.noreply.github.com>
2025-07-22 18:45:38 -0400

    Update Grenade.cs
    Added OnCollisionEnter  and altered start to incorporate sticky bomb mode.

--- Assets/Scripts/Grenade.cs
+++ Assets/Scripts/Grenade.cs
@@ -11,18 +11,57 @@ public class Grenade : MonoBehaviour
 
     [SerializeField] private GameObject explosionPrefab;
 
-
+    [SerializeField] private bool OnStickyBomb;
+    [SerializeField] private bool isTracking;
+    bool OnSurface;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        grenadeRigidB.linearVelocity = (transform.forward * grenadeSpeed) + (transform.up * grenadeSpeedY);
-        StartCoroutine(explode());
+        if (!OnStickyBomb)
+        {
+            grenadeRigidB.useGravity = true;
+            grenadeRigidB.linearVelocity = (transform.forward * grenadeSpeed) + (transform.up * grenadeSpeedY);
+            StartCoroutine(explode());
+        }
+        else
+        {
+            grenadeRigidB.useGravity = false;
+            grenadeRigidB.linearVelocity = (transform.forward * grenadeSpeed);
+            StartCoroutine(explode());
+        }
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isTracking)
+        {
+            grenadeRigidB.useGravity = false;
+            grenadeRigidB.linearVelocity = gamemanager.instance;
+        } else if(isTracking && OnStickyBomb)
+        {
+
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (OnStickyBomb && !OnSurface)
+        {   
+            //Making it stationary
+            grenadeRigidB.isKinematic = true;
+
+            grenadeRigidB.linearVelocity = Vector3.zero;
+            grenadeRigidB.angularVelocity = Vector3.zero;
+
+
+            //Making it stick to a surface; Moving with the object it parents
+            transform.SetParent(collision.transform);
+
+            //Setting it true that it is on a surface
+            OnSurface = true;
+        }
     }
 
     IEnumerator explode()
