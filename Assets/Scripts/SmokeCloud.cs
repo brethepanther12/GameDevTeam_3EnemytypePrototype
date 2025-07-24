753c117fd2f125ceb210ff914a3e5c2c4af74b0d
brethepanther12 <155138278+brethepanther12@users.noreply.github.com>
2025-07-24 05:04:16 -0400

    smoke grenade
    Starting smoke test

--- Assets/Scripts/SmokeCloud.cs
+++ Assets/Scripts/SmokeCloud.cs
@@ -1,16 +1,54 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class SmokeCloud : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    // Duration the smoke lasts in seconds
+    [SerializeField] public float smokeDuration;
+
+    // List to keep track of enemies inside the cloud
+    private List<FlyingAI> enemiesInSmoke = new List<FlyingAI>();
+
+    private void Start()
     {
-        
+        // Destroy the smoke cloud after duration
+        Destroy(gameObject, smokeDuration);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.CompareTag("Enemy"))
+        {
+            FlyingAI enemy = other.GetComponent<FlyingAI>();
+            if (enemy != null && !enemiesInSmoke.Contains(enemy))
+            {
+                enemiesInSmoke.Add(enemy);
+                enemy.SetInvisible(true);
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            FlyingAI enemy = other.GetComponent<FlyingAI>();
+            if (enemy != null && enemiesInSmoke.Contains(enemy))
+            {
+                enemiesInSmoke.Remove(enemy);
+                enemy.SetInvisible(false);
+            }
+        }
+    }
+
+    private void OnDestroy()
+    {
+        // When smoke disappears, reset invisibility on all enemies inside
+        foreach (var enemy in enemiesInSmoke)
+        {
+            if (enemy != null)
+                enemy.SetInvisible(false);
+        }
+        enemiesInSmoke.Clear();
     }
 }
