62efe841cde195ea3269847a1c50fdf85857f3d0
brethepanther12 <155138278+brethepanther12@users.noreply.github.com>
2025-08-12 18:48:46 -0400

    Update DifficultyMenuFunctions.cs

--- Assets/Scenes/Pro-Wife/DifficultyMenuFunctions.cs
+++ Assets/Scenes/Pro-Wife/DifficultyMenuFunctions.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using TMPro;
+using System.Collections.Generic;
 
 public class DifficultyMenuFunctions : MonoBehaviour
 {
@@ -8,12 +9,46 @@ public class DifficultyMenuFunctions : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        RefreshDropdown();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void RefreshDropdown()
     {
-        
+        difficultyDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        gamemanager.DifficultyLevels[] difficulties = new gamemanager.DifficultyLevels[]
+        {
+            gamemanager.DifficultyLevels.easy,
+            gamemanager.DifficultyLevels.normal,
+            gamemanager.DifficultyLevels.hard
+        };
+
+        for (int i = 0; i < difficulties.Length; i++)
+        {
+            gamemanager.DifficultyLevels difficulty = difficulties[i];
+
+            bool locked = gamemanager.instance.IsDifficultyLocked(difficulty);
+
+            string label = difficulty.ToString();
+
+            if (locked)
+            {
+                label = label + " (Locked)";
+            }
+
+            options.Add(label);
+        }
+
+        difficultyDropdown.AddOptions(options);
+        difficultyDropdown.value = (int)gamemanager.instance.currentDifficulty;
+        difficultyDropdown.RefreshShownValue();
+    }
+
+    public void OnDifficultyChanged(int index)
+    {
+        gamemanager.instance.SetDifficulty((gamemanager.DifficultyLevels)index);
+        RefreshDropdown();
     }
 }
