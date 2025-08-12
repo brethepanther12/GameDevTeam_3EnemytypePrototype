e973ad7f6712d3a9d0f76468868ab24936ec36cc
brethepanther12 <155138278+brethepanther12@users.noreply.github.com>
2025-08-12 18:23:59 -0400

    Update DifficultyMenuFunctions.cs

---
+++ Assets/Scenes/Pro-Wife/DifficultyMenuFunctions.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using TMPro;
+
+public class DifficultyMenuFunctions : MonoBehaviour
+{
+    [SerializeField] private TMP_Dropdown difficultyDropdown;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
