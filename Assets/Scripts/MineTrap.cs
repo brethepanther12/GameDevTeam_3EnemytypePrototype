df7106c5eca16b13c19964ceb6858d398cf189e1
Travis-Huth94 <travishuth84@gmail.com>
2025-08-14 19:45:08 -0500

    Mine trap created
    Just needs explosion and audio but overall it is finished and works as intended.

---
+++ Assets/Scripts/MineTrap.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class MineTrap : MonoBehaviour
+{
+    [SerializeField] int damageAmount = 50;
+    [SerializeField] GameObject explosionEffect;
+    [SerializeField] bool destroyAfterTrigger = true;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Mine triggered!");
+
+            playerController player = other.GetComponent<playerController>();
+            if (player != null)
+                player.takeDamage(damageAmount);
+
+            if (explosionEffect != null)
+                Instantiate(explosionEffect, transform.position, Quaternion.identity);
+
+            if (destroyAfterTrigger)
+                Destroy(gameObject);
+        }
+    }
+}
