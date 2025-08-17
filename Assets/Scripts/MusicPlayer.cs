fbaffc2e7d643ba916b450c352b47595c8ce2421
FinsofFury <jowersjack@gmail.com>
2025-08-17 02:48:56 -0400

    Music now shuffles a playlist of songs depending on if you're in the main menu, a normal level, or the boss level

--- Assets/Scripts/MusicPlayer.cs
+++ Assets/Scripts/MusicPlayer.cs
@@ -1,26 +1,30 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+using System.Linq;
 
 public class MusicPlayer : MonoBehaviour
 {
     public static MusicPlayer instance;
 
-    [SerializeField] AudioClip mainMenuMusic;
-    [SerializeField] AudioClip gameMusic;
-    [SerializeField] AudioClip levelOne;
-    [SerializeField] AudioClip bossFight;
+    [SerializeField] List<AudioClip> mainMenuTracks;
+    [SerializeField] List<AudioClip> gameplayTracks;
+    [SerializeField] List<AudioClip> bossFightTracks;
 
-    AudioSource audioSource;
+    private AudioSource audioSource;
+    private List<AudioClip> currentPlaylist = new List<AudioClip>();
+    private List<AudioClip> shuffledPlaylist = new List<AudioClip>();
+    
     public float Volume
     {
-        get => audioSource != null ? audioSource.volume : 1f;
+        get => audioSource != null ? audioSource.volume : .5f;
         set
         {
             if (audioSource != null)
                 audioSource.volume = Mathf.Clamp01(value);
         }
-    }
 
+    }
     void Awake()
     {
         if (instance != null && instance != this)
@@ -30,40 +34,72 @@ public class MusicPlayer : MonoBehaviour
         }
         instance = this;
         DontDestroyOnLoad(gameObject);
-
         audioSource = GetComponent<AudioSource>();
         SceneManager.sceneLoaded += OnSceneLoaded;
     }
 
+    void Start()
+    {
+        Volume = PlayerPrefs.GetFloat("MusicVolume", 0.5f);
+        Scene currentScene = SceneManager.GetActiveScene();
+        TriggerMusicForScene(currentScene.name);
+    }
 
     void OnSceneLoaded(Scene scene, LoadSceneMode mode)
     {
-        if (scene.name == "MainMenu")
+        TriggerMusicForScene(scene.name);
+    }
+
+    void TriggerMusicForScene(string sceneName)
+    {
+        shuffledPlaylist.Clear();
+
+        if (sceneName == "Main Menu")
         {
-            PlayMusic(mainMenuMusic);
+            currentPlaylist = mainMenuTracks;
         }
-        else if (scene.name == "Tutorial")
+        else if (sceneName == "Travis-(Boss enemy)")
         {
-            PlayMusic(gameMusic);
+            currentPlaylist = bossFightTracks;
         }
-        else if (scene.name == "level 1")
+        else
         {
-            PlayMusic(levelOne);
+            currentPlaylist = gameplayTracks;
         }
-        else if( scene.name == "Boss Fight")
+
+        PlayNextTrack();
+    }
+
+    void PlayNextTrack()
+    {
+        if (currentPlaylist.Count == 0) return;
+
+        if (shuffledPlaylist.Count == 0)
         {
-            PlayMusic(bossFight);
+            shuffledPlaylist = currentPlaylist.OrderBy(a => Random.value).ToList();
         }
-        
+
+        AudioClip clipToPlay = shuffledPlaylist.Last();
+        shuffledPlaylist.RemoveAt(shuffledPlaylist.Count - 1);
+        PlayMusic(clipToPlay);
+    }
+
+    void Update()
+    {
+        if (audioSource.isPlaying || currentPlaylist.Count == 0)
+        {
+            return;
+        }
+
+        PlayNextTrack();
     }
 
     void PlayMusic(AudioClip clip)
     {
-        if (audioSource.clip == clip)
-            return;
+        if (clip == null) return;
 
         audioSource.clip = clip;
-        audioSource.loop = true;
+        audioSource.loop = false;
         audioSource.Play();
     }
 }
