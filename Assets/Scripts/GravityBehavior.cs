0f8053cfcbdc6cb62d949c1bfbe809bb17e60aca
Christian Lykins <CALykins@student.fullsail.edu>
2025-08-17 21:09:34 -0400

    Gravity Volume Fixed

--- Assets/Scripts/GravityBehavior.cs
+++ Assets/Scripts/GravityBehavior.cs
@@ -3,31 +3,29 @@ using UnityEngine;
 public class GravityBehavior : MonoBehaviour
 {
 
-    public playerController controller;
     private float storedGravity;
-    public float gravityInVolume;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        if (controller == null)
-        {
-            controller = gamemanager.instance.playerScript;
-        }
-        
-    }
+    public float gravityInVolume = -2f;
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (!other.CompareTag("Player")) return;
+
+        playerController controller = other.GetComponent<playerController>();
+        if (controller != null)
         {
             storedGravity = controller.GetGravity();
+            controller.SetGravity(gravityInVolume);
         }
-
-        controller.SetGravity(gravityInVolume);
     }
 
     private void OnTriggerExit(Collider other)
     {
-        controller.SetGravity(storedGravity);
+        if (!other.CompareTag("Player")) return;
+
+        playerController controller = other.GetComponent<playerController>();
+        if (controller != null)
+        {
+            controller.SetGravity(storedGravity);
+        }
     }
 }
