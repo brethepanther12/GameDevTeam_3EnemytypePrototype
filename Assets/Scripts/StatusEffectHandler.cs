81f3a64930bc48280619305bba81a3915bcdcf05
Christian Lykins <CALykins@student.fullsail.edu>
2025-08-17 22:08:40 -0400

    Update

--- Assets/Scripts/StatusEffectHandler.cs
+++ Assets/Scripts/StatusEffectHandler.cs
@@ -16,14 +16,14 @@ public class StatusEffectHandler : MonoBehaviour
 
         if (type == DamageStatus.None)
         {
-            Debug.Log($"Skipping status {type}");
+            //Debug.Log($"Skipping status {type}");
             return;
         }
 
         // Prevent duplicate effect of same type
         if (activeEffects.ContainsKey(type))
         {
-            Debug.Log($"Refreshing status {type}");
+            //Debug.Log($"Refreshing status {type}");
 
             if (activeEffects[type] != null)
             {
@@ -44,40 +44,40 @@ public class StatusEffectHandler : MonoBehaviour
         switch (data.statusType)
         {
             case DamageStatus.Fire:
-                Debug.LogWarning("Fire status effect applied");
+                //Debug.LogWarning("Fire status effect applied");
                 
                 break;
 
             case DamageStatus.Corrosive:
-                Debug.LogWarning("Corrosive status effect applied");
+                //Debug.LogWarning("Corrosive status effect applied");
                 
                 break;
 
             case DamageStatus.Cryo:
 
-                Debug.LogWarning("Cryo status effect applied");
+                //Debug.LogWarning("Cryo status effect applied");
                 target.slowDown(data.slowDownMagnitude, data.statusDuration);
                 break;
 
             case DamageStatus.Electric:
 
-                Debug.LogWarning("Electric status effect applied");
+                //Debug.LogWarning("Electric status effect applied");
                 target.slowDown(data.slowDownMagnitude, data.statusDuration);
                 break;
 
             case DamageStatus.Explosive:
 
-                Debug.LogWarning("Explosive status effect applied");
+                //Debug.LogWarning("Explosive status effect applied");
                 
                 break;
 
             case DamageStatus.Plasma:
 
-                Debug.LogWarning("Plasma burn status effect applied");
+                //Debug.LogWarning("Plasma burn status effect applied");
                 break;
 
             default:
-                Debug.LogWarning($"Unhandled status effect: {data.statusType}");
+                //Debug.LogWarning($"Unhandled status effect: {data.statusType}");
                 yield break;
         }
 
