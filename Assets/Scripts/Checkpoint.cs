82239c65be8447bf80733c4b5d2dbb18fcddcc02
FinsofFury <jowersjack@gmail.com>
2025-08-24 05:19:18 -0400

    Added sounds for checkpoint and grapple

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -3,12 +3,18 @@ using System.Collections;
 
 public class Checkpoint : MonoBehaviour
 {
+    [SerializeField] private AudioClip checkpointSound;
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player") && gamemanager.instance.PlayerSpawnPOS.transform.position != transform.position)
         {
             gamemanager.instance.PlayerSpawnPOS.transform.position = transform.position;
             gamemanager.instance.PlayerSpawnPOS.transform.localRotation = transform.localRotation;
+
+            if (checkpointSound != null)
+            {
+                AudioSource.PlayClipAtPoint(checkpointSound, transform.position);
+            }
 
             SavePlayerState(other.GetComponent<playerController>());
 
