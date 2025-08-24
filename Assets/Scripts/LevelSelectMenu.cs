1d2468b705589a9fb00a1f3075598c96c7c27b05
FinsofFury <jowersjack@gmail.com>
2025-08-24 02:23:00 -0400

    fixed quit menu location in main menu. also took away the locks for level select.

--- Assets/Scripts/LevelSelectMenu.cs
+++ Assets/Scripts/LevelSelectMenu.cs
@@ -11,31 +11,18 @@ public class LevelSelectMenu : MonoBehaviour
     public class LevelEntry
     {
         public string sceneName;
-        public int levelNumberToUnlock;
         public Button levelButton;
-        public GameObject lockIcon;
     }
 
     public List<LevelEntry> levelEntries;
 
     void Start()
     {
-        int highestLevelUnlocked = PlayerPrefs.GetInt("LevelsUnlocked", 1);
-
         foreach (var entry in levelEntries)
         {
-            bool isUnlocked = highestLevelUnlocked >= entry.levelNumberToUnlock;
+            entry.levelButton.interactable = true;
 
-            entry.levelButton.interactable = isUnlocked;
-            if (entry.lockIcon != null)
-            {
-                entry.lockIcon.SetActive(!isUnlocked);
-            }
-
-            if (isUnlocked)
-            {
-                entry.levelButton.onClick.AddListener(() => LoadLevel(entry.sceneName));
-            }
+            entry.levelButton.onClick.AddListener(() => LoadLevel(entry.sceneName));
         }
     }
 
