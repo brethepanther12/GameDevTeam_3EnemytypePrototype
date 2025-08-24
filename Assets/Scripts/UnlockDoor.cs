b6b0fb8e3037d17337711684e8d2a71cd99fd94b
FinsofFury <jowersjack@gmail.com>
2025-08-24 04:46:19 -0400

    locked doors have sound. unlocking doors has sound

--- Assets/Scripts/UnlockDoor.cs
+++ Assets/Scripts/UnlockDoor.cs
@@ -7,6 +7,9 @@ public class UnlockDoor : MonoBehaviour
     public GameObject doorObject;
     private bool isUnlocked = false;
 
+    [SerializeField] private AudioClip lockedDoorSound;
+    [SerializeField] private AudioClip unlockSound;
+
     private void OnTriggerEnter(Collider other)
     {
         if (isUnlocked || !other.CompareTag("Player"))
@@ -22,10 +25,19 @@ public class UnlockDoor : MonoBehaviour
         {
             Debug.Log("You do not have the required items!");
         }
+        if (lockedDoorSound != null)
+        {
+            AudioSource.PlayClipAtPoint(lockedDoorSound, transform.position);
+        }
     }
 
     private void Unlock(PlayerInventory inventory)
     {
+        if (unlockSound != null)
+        {
+            AudioSource.PlayClipAtPoint(unlockSound, transform.position);
+        }
+
         isUnlocked = true;
 
         foreach (ItemSO item in requiredItems)
