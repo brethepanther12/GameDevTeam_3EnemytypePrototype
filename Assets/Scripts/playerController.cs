ce7df968dc5d17e6cf0c0365b91a4ab086a3b7df
Christian Lykins <CALykins@student.fullsail.edu>
2025-08-26 21:11:28 -0400

    Reapply "Reload"
    This reverts commit ac50c69ad17f89a5c7ac33862c7a7be0c16b7654.

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -102,6 +102,7 @@ public class playerController : MonoBehaviour, IDamage, Visibility
     public GameObject meleeTrigger;
     bool isAttacking;
     private bool isDead = false;
+
     void Start()
     {
         originalSpeed = this.speed;
@@ -628,7 +629,7 @@ public class playerController : MonoBehaviour, IDamage, Visibility
             shield = shieldOrig;
             armor = armorOrig;
         }
-
+        
         PlayerInventory inventory = GetComponent<PlayerInventory>();
         if (inventory != null)
         {
